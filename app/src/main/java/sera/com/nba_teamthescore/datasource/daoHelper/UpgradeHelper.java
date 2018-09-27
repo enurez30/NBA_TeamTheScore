8f919ab6436fd81460d214843abd64abf9ea1149
Sera <sergey.phursov@gmail.com>
2018-09-27 13:37:12 -0300

    q

--- app/src/main/java/sera/com/nba_teamthescore/datasource/daoHelper/UpgradeHelper.java
+++ app/src/main/java/sera/com/nba_teamthescore/datasource/daoHelper/UpgradeHelper.java
@@ -8,19 +8,19 @@ import sera.com.nba_teamthescore.objects.DaoMaster;
 
 public class UpgradeHelper extends DaoMaster.OpenHelper {
 
-    public UpgradeHelper(Context context, String name) {
+public UpgradeHelper(Context context, String name) {
         super(context, name);
-    }
+        }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+@Override
+public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         super.onUpgrade(db, oldVersion, newVersion);
         Log.d("DEBUG", "DB_OLD_VERSION : " + oldVersion + ", DB_NEW_VERSION : " + newVersion);
         switch (oldVersion) {
-            case 1:
-            case 2:
-                //db.execSQL("ALTER TABLE " + UserDao.TABLENAME + " ADD COLUMN " + UserDao.Properties.Name.columnName + " TEXT DEFAULT 'DEFAULT_VAL'");
+        case 1:
+        case 2:
+        //db.execSQL("ALTER TABLE " + UserDao.TABLENAME + " ADD COLUMN " + UserDao.Properties.Name.columnName + " TEXT DEFAULT 'DEFAULT_VAL'");
         }
-    }
+        }
 
-}
+        }
